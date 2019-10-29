143870fdb513f4ce82f17e757f09d26540602406
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2019-10-29 23:26:58 +0200

    Ferestre conectare si inregistrare

--- app/src/main/java/com/example/sportspot/SportsListActivity.java
+++ app/src/main/java/com/example/sportspot/SportsListActivity.java
@@ -3,14 +3,28 @@ package com.example.sportspot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 public class SportsListActivity extends AppCompatActivity {
+    private ListView sportsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sports_list);
+        initComponents();
 
+        ArrayAdapter<CharSequence> sportsAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.lv_sports_list,
+                android.R.layout.simple_list_item_1);
+        sportsListView.setAdapter(sportsAdapter);
 
     }
+
+    private void initComponents(){
+        sportsListView = findViewById(R.id.sports_list_lv);
+    }
+
+
 }
