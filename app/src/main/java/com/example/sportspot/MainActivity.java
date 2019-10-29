58d96d49880ca6d5d02e6929e10448681c37602f
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2019-10-29 21:15:52 +0200

    SplashScreen

--- app/src/main/java/com/example/sportspot/MainActivity.java
+++ app/src/main/java/com/example/sportspot/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.sportspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIMEOUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginIntent = new Intent(MainActivity.this, SportsListActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
     }
 }
---
+++ app/src/main/java/com/example/sportspot/SportsListActivity.java
@@ -0,0 +1,16 @@
+package com.example.sportspot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SportsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sports_list);
+
+
+    }
+}
