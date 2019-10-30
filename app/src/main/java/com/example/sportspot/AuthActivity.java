926246388f9860ccd78ea7138a0e4a3640b9d7da
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2019-10-30 12:48:30 +0200

    Initializare componente vizuale ferestre conectare si inregistrare

--- app/src/main/java/com/example/sportspot/AuthActivity.java
+++ app/src/main/java/com/example/sportspot/AuthActivity.java
@@ -3,12 +3,28 @@ package com.example.sportspot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class AuthActivity extends AppCompatActivity {
+    private EditText auth_username;
+    private EditText auth_password;
+    private Button auth_connect;
+    private TextView auth_signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        initComponents();
+    }
+
+    private void initComponents(){
+        auth_username = findViewById(R.id.auth_username);
+        auth_password = findViewById(R.id.auth_password);
+        auth_connect = findViewById(R.id.auth_connect_button);
+        auth_signup = findViewById(R.id.auth_signup_button);
     }
 }
--- app/src/main/java/com/example/sportspot/SignupActivity.java
+++ app/src/main/java/com/example/sportspot/SignupActivity.java
@@ -3,12 +3,30 @@ package com.example.sportspot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+    private EditText signup_username;
+    private EditText signup_password;
+    private EditText signup_confirm_password;
+    private Button signup_create_account_button;
+    private TextView signup_auth_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        initComponents();
+    }
+
+    private void initComponents(){
+        signup_username = findViewById(R.id.signup_username);
+        signup_password = findViewById(R.id.signup_password);
+        signup_confirm_password = findViewById(R.id.signup_confirm_password);
+        signup_create_account_button = findViewById(R.id.signup_create_account_button);
+        signup_auth_button = findViewById(R.id.signup_auth_button);
+
     }
 }
