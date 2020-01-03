51c15d6d476e07eed76f3c95c5ee0c3b7e996db1
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2020-01-04 01:43:17 +0200

    Redirectionare pdm.ase.ro din contact

--- app/src/main/java/com/example/sportspot/ContactActivity.java
+++ app/src/main/java/com/example/sportspot/ContactActivity.java
@@ -2,9 +2,13 @@ package com.example.sportspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.sportspot.util.Const;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,6 +21,7 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ContactActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private TextView visitSite;
 
     private GoogleMap gMap;
 
@@ -24,6 +29,16 @@ public class ContactActivity extends AppCompatActivity implements OnMapReadyCall
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        visitSite = findViewById(R.id.contact_site);
+
+        visitSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://pdm.ase.ro/"));
+                startActivity(intent);
+            }
+        });
 
         SupportMapFragment  mapView = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.contact_map);
         mapView.getMapAsync(this);
@@ -33,7 +48,7 @@ public class ContactActivity extends AppCompatActivity implements OnMapReadyCall
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
         LatLng csie = new LatLng(44.447867,26.0991195);
-        gMap.addMarker(new MarkerOptions().position(csie).title("CSIE ASE"));
+        gMap.addMarker(new MarkerOptions().position(csie).title(getString(R.string.csie_ase)));
         gMap.moveCamera(CameraUpdateFactory.newLatLng(csie));
         gMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f));
     }
